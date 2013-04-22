ae499f82d8905a2b713428a7012362eed16ddfa8
jt1134 <jt1134@gmail.com>
2013-04-21 21:19:05 -0500

    remove extra languages, Email and VideoEditor apps
    Change-Id: I76234def1d464bbadefe503738119bed36e61519

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -21,16 +21,7 @@
 
 PRODUCT_PACKAGES := \
     libfwdlockengine \
-    VideoEditor \
     WAPPushManager
-
-
-PRODUCT_PACKAGES += \
-    libvideoeditor_jni \
-    libvideoeditor_core \
-    libvideoeditor_osal \
-    libvideoeditor_videofilters \
-    libvideoeditorplayer
 
 PRODUCT_PACKAGES += \
     Galaxy4 \
@@ -55,11 +46,8 @@ PRODUCT_LOCALES := en_US
 # Get some sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
-# Get the TTS language packs
-$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
-
-# Get a list of languages.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
+# Get the TTS language pack
+$(call inherit-product-if-exists, external/svox/pico/lang/PicoLangEnUsInSystem.mk)
 
 # Get everything else from the parent package
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
