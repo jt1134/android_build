72edae3e925b6d3ff0decf6eb24788e13ec928f9
jt1134 <jt1134@gmail.com>
2013-08-11 04:36:33 -0500

    Revert "remove extra languages, Email and VideoEditor apps"
    This reverts commit ae499f82d8905a2b713428a7012362eed16ddfa8.

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -21,7 +21,16 @@
 
 PRODUCT_PACKAGES := \
     libfwdlockengine \
+    VideoEditor \
     WAPPushManager
+
+
+PRODUCT_PACKAGES += \
+    libvideoeditor_jni \
+    libvideoeditor_core \
+    libvideoeditor_osal \
+    libvideoeditor_videofilters \
+    libvideoeditorplayer
 
 PRODUCT_PACKAGES += \
     Galaxy4 \
@@ -46,8 +55,11 @@ PRODUCT_LOCALES := en_US
 # Get some sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
-# Get the TTS language pack
-$(call inherit-product-if-exists, external/svox/pico/lang/PicoLangEnUsInSystem.mk)
+# Get the TTS language packs
+$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
+
+# Get a list of languages.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 
 # Get everything else from the parent package
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
